f6c76d2a265577e65e4e7d425e3acd0a9e299df2
Prajjwal Tripathi <tprajjwal22@gmail.com>
2023-02-19 21:30:56 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static class A{
+        public static String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public static String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main (String args[])
+    {
+        B b = new B();
+        b.meth();
+
+        B b1 = new B();
+        b1.meth();
+    }
   
 }
